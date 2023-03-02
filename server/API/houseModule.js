38802a2c1173cc5e52c5d0d2dc2f7c16a44ce17f
maoHangFei <3061148499@qq.com>
2023-03-02 16:50:35 +0800

    完成接口

---
+++ server/API/houseModule.js
@@ -0,0 +1,38 @@
+// 用于操作数据库
+let db = require('../db/index')
+let makeUuid = require("../utils/makeUuid")
+
+// GET 查询数据
+// 需要传入的参数 =》 名称：city （都不传则默认所有数据）
+exports.getHouseTableByCity = (req, res) => {
+    let sql = 'SELECT * FROM housetable';
+    let obj = req.query;
+    if (obj.name) sql = `SELECT * FROM housetable WHERE city = "${obj.city}"`
+    db.query(sql, (err, data, fields) => {
+        if (err) {
+            return res.send('错误：' + err.message)
+        }
+        res.send({
+            status: 200,
+            message: "success",
+            data
+        });
+    })
+}
+
+// GET 获取单个点位的详情接口
+// 需要传入的参数 =》 id：id
+exports.getHouseDetail = (req, res) => {
+    // 根据条件查询数据库表单的数据
+    const sql = `SELECT * FROM housetable WHERE id = '${req.query.id}'`
+    db.query(sql, (err, data, fields) => {
+        if (err) {
+            return res.send('错误：' + err.message)
+        }
+        res.send({
+            status: 200,
+            message: "success",
+            data
+        });
+    })
+}
