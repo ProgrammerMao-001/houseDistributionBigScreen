6045e9b8860c760f48c81b921c6443e67ff1c479
maoHangFei <3061148499@qq.com>
2023-03-03 03:30:49 +0800

    del

--- src/views/centerMap/centerMap.vue
+++ src/views/centerMap/centerMap.vue
@@ -1,20 +1,17 @@
 <template>
     <div style="width: 100%;height: 100%;">
         <div id="centerMap" class="centerMap" style="width: 100%;height: 100%"></div>
-<!--        <projectBasicInfoDialog ref="projectBasicInfoDialog"/>-->
+        <!--        <projectBasicInfoDialog ref="projectBasicInfoDialog"/>-->
     </div>
 </template>
 
 <script>
     // import projectBasicInfoDialog from "@/components/projectBasicInfoDialog";
     import MiddleUtils from "@/utils/middle-utils"
-    import { getHouseTableByCity } from "@/api/houseModule";
+    import {getHouseTableByCity} from "@/api/houseModule";
 
     var coordtransform = require('coordtransform');
     let map;
-    // const zoom = 14;
-    // const handler = [];
-    // let viewer;
     var T = window.T;
 
     export default {
@@ -22,9 +19,6 @@
         components: {},
         data() {
             return {
-                // lon: 109.79145541962566, // 经度
-                // lat: 32.970373734783635, // 纬度
-                // zoom: 7, // 缩放比例
                 lon: 0,
                 lat: 0,
                 zoom: 16,
@@ -37,68 +31,29 @@
             showMap() {
                 const imageURL = 'http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=4c3a4b8cec3d4af47a12cdd192ce35cb';
                 //创建自定义图层对象
-                // const lay = new T.TileLayer(imageURL, {minZoom: 13, maxZoom: 18});
                 const lay = new T.TileLayer(imageURL, {minZoom: 0, maxZoom: 18});
                 map = new T.Map('centerMap', {layers: [lay]});
 
 
-                const typeArr= [
+                const typeArr = [
                     {
-                        // title: '卫星',
-                        title: '',
+                        title: '卫星',
                         icon: ' http://api.tianditu.gov.cn/v4.0/image/map/maptype/satellite.png',
                         layer: TMAP_SATELLITE_MAP,
                     },
                     {
-                        // title: '地图', //地图控件上所要显示的图层名称
-                        title: '', //地图控件上所要显示的图层名称
+                        title: '地图', //地图控件上所要显示的图层名称
                         icon: 'http://api.tianditu.gov.cn/v4.0/image/map/maptype/vector.png', //地图控件上所要显示的图层图标（默认图标大小80x80）
                         layer: TMAP_NORMAL_MAP, //地图类型对象，即MapType。
                     },
-                    // {
-                    //   title: '卫星混合',
-                    //   icon: 'http://api.tianditu.gov.cn/v4.0/image/map/maptype/satellitepoi.png',
-                    //   layer: TMAP_HYBRID_MAP,
-                    // },
-                    // {
-                    //   title: "地形",
-                    //   icon: " http://api.tianditu.gov.cn/v4.0/image/map/maptype/terrain.png",
-                    //   layer: "TMAP_TERRAIN_MAP"
-                    // },
-                    // {
-                    //   title: "地形混合",
-                    //   icon: " http://api.tianditu.gov.cn/v4.0/image/map/maptype/terrainpoi.png",
-                    //   layer: "TMAP_TERRAIN_HYBRID_MAP"
-                    // }
                 ];
                 const ctrl = new T.Control.MapType(typeArr);
                 //添加控件
                 map.addControl(ctrl);
-
-
-
-                // console.log(map);
-                // map.centerAndZoom(new T.LngLat(this.lon, this.lat), this.zoom);
-                // map.centerAndZoom(new T.LngLat(31.898748, 120.27348), 11);
-                // map.addEventListener('maptypechange', (e) => {
-                //     // console.log(e);
-                //     //e.mapType 就是 当前切换的MapType 配置的地图
-                //     if (e.mapType.name == 'TMAP_NORMAL_MAP') {
-                //         // 普通地图 覆盖图隐藏
-                //         this.img_nj.hide();
-                //         this.img_jzs.hide();
-                //     } else {
-                //         this.img_nj.show();
-                //         this.img_jzs.show();
-                //     }
-                // });
                 this.getProjectList();
-                // map.setMinZoom(4); // 最小的缩放比例
-                // map.setMaxZoom(18); // 最大的缩放比例
                 setTimeout(() => {
                     map.centerAndZoom(new T.LngLat(this.lon, this.lat), this.zoom);
                 }, 500)
-                // map.setMaxBounds(new T.LngLatBounds(new T.LngLat(106.141401, 33.441127), new T.LngLat(107.203184, 27.675892))); // 限制展示的区域
             },
 
             // 获取地图数据的接口
@@ -106,8 +61,6 @@
                 let lngArr = []; // 存放所有经度
                 let latArr = []; // 存放所有纬度
                 let lays = map.getOverlays();//获取地图上所有的覆盖物
-                // console.log(lays)
-                // var s = 0;
                 for (let i = lays.length - 1; i >= 0; i--) { //倒序删除避免长度发生变化
                     map.removeOverLay(lays[i]); //从地图上移除。
                 }
@@ -124,7 +77,7 @@
                                 let gcj02towgs84 = coordtransform.gcj02towgs84(item.longitude, item.latitude)
                                 const marker = new T.Marker(new T.LngLat(gcj02towgs84[0], gcj02towgs84[1]), {
                                     icon: icon,
-                                    title: item.name + ' - ' + item.address
+                                    title: item.housename + ' - ' + item.address
                                 });
                                 // map.removeOverLay(marker); // 清空点位
                                 map.addOverLay(marker); // 打点
@@ -148,14 +101,9 @@
                         latArr.sort(function (a, b) {
                             return b - a;
                         })
-                        // console.log(lngArr[0], lngArr[lngArr.length - 1]); // 经度 最大值 最小值
-                        // console.log(latArr[0], latArr[latArr.length - 1]); // 纬度 最大值 最小值
                         this.lon = (parseFloat(lngArr[0]) + parseFloat(lngArr[lngArr.length - 1])) / 2;
                         this.lat = (parseFloat(latArr[0]) + parseFloat(latArr[latArr.length - 1])) / 2;
                         this.zoom = this.getZoom(lngArr[0], lngArr[lngArr.length - 1], latArr[0], latArr[latArr.length - 1]);
-
-                        // if (this.lon - this.lat < 90) this.zoom = 16;
-                        // console.log(this.lon, this.lat, this.zoom);
                     } else {
                         this.$message.warning('获取地图数据失败！');
                     }
@@ -169,9 +117,6 @@
              * 而这些差值与级别的对应关系，以及经纬差值之间的计算比例，是经过了我不少的调试得出来的。 也是花了一点功夫的。
              * 越是显示级别大的时候，越小的差值引起的误差就可能越大。所以我们这里默认最大的显示级别为14。
              * 也就是说，如果差值比我们差值数组中最小的差值还小的话，我们就返回14。
-             ————————————————
-             版权声明：本文为CSDN博主「liusaint1992」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-             原文链接：https://blog.csdn.net/liusaint1992/article/details/48935659
              * */
             getZoom(maxJ, minJ, maxW, minW) {
                 if (maxJ == minJ && maxW == minW) return 13;
@@ -193,28 +138,11 @@
         mounted() {
             this.showMap();
 
-          // 接收中间件，改变报警状态
-          MiddleUtils.$on('changeAlarmStatus', (value) => {
-            this.alarmStatus = value.alarmStatus;
-            this.getProjectList();
-          })
-
-            // 向地图上添加标注 demo 写死的
-            // var marker = new T.Marker(new T.LngLat(109.67291896206848, 32.855915983379035));
-            // map.addOverLay(marker);
-            // 向地图上添加标注 demo 写死的
-
-            // 清空地图上所有的点，实现userNow变化时重新打点
-            // setTimeout(() => {
-            //     let lays = map.getOverlays();//获取地图上所有的覆盖物
-            //     console.log(lays)
-            //     // var s = 0;
-            //     for (let i = lays.length - 1; i >= 0; i--) { //倒序删除避免长度发生变化
-            //         map.removeOverLay(lays[i]); //从地图上移除。
-            //     }
-            // }, 500)
-            // 清空地图上所有的点，实现userNow变化时重新打点
-
+            // 接收中间件，改变报警状态
+            MiddleUtils.$on('changeAlarmStatus', (value) => {
+                this.alarmStatus = value.alarmStatus;
+                this.getProjectList();
+            })
         },
         created() {
         }
@@ -227,11 +155,12 @@
         height: 40px;
         border-radius: 8px;
     }
+
     ::v-deep .tdt-right .tdt-control {
-       margin-right: 180px;
+        margin-right: 180px;
     }
 
     ::v-deep .tdt-top .tdt-control {
-       margin-top: 4px;
+        margin-top: 4px;
     }
 </style>
